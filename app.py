79b84785f0ee7565b4e236e500dfd354118e7a1b
gayathrig22beds <gayathrig.22.beds@acharya.ac.in>
2026-04-26 23:12:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@
 import pickle
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
 import streamlit as st
 from sklearn.preprocessing import MinMaxScaler
